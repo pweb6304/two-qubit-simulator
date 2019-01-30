2d14ce5a215e5cc0ce4c2352d9622c3a74fe433c
pweb6304 <pweb6304@uni.sydney.edu.au>
2019-01-30 16:25:37 +1100

    fixed measurement

--- measurement.py
+++ measurement.py
@@ -2,9 +2,10 @@ import numpy as np
 
 def measure(state):
     """Measures the qubit state in the z-basis. Returns the measurement outcome (1 or -1)"""
-    prob_1 = abs(state[1])**2
+    prob_1 = abs(state[0])**2 #calculates probability of measurement outcome of one
     comparison_for_prob=np.random.rand()
-    if prob_1<comparison_for_prob:
+    #compares a random number to the probability of outcome of one. Iff number is smaller, return one.
+    if prob_1>comparison_for_prob: 
         outcome=1
     else:
         outcome=-1
